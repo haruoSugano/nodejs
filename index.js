82603842fc790baec71a58465dab2cb2dc9132ca
haruoSugano <haruosugano@icloud.com>
2022-09-12 21:27:05 -0300

    get

--- index.js
+++ index.js
@@ -1,13 +1,17 @@
 const express = require('express');
+const bodyParser = require('body-parser');
+const usuarios = require('./usuarios.json');
 const app = express();
 
-app.use(express.json());
+app.use(bodyParser.json());
 
-const usuario = {
-    id: 123456,
-    nome: "helio",
-    email: "helio@gmail.com",
-}
+app.get("/rede/usuarios", (req, res) => {
+    res.json(usuarios);
+});
+
+app.post("rede/usuarios", (req, res) => {
+    console.log("POST");
+});
 
 app.listen(4000,() => {
     console.log("Porta: 4000");
